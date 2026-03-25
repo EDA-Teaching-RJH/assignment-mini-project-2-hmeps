c0004b9097c7b48750ecf3c3a3c0bd394cabe6c1
hmeps <hm18@kent.ac.uk>
2026-03-25 12:18:21 +0000

    Second commit Added regex library to simulate a normal password checker that covers not only length but characters included as well! Increased the length of the while loop so that it needs all conditions to be met before it says that it is a strong password Used is_strong to validate all the conditions in one function so it saves time and line space and is more readable for a user.

--- passwordChecker.py
+++ passwordChecker.py
@@ -1,13 +1,26 @@
+import re
 class PasswordStrengthChecker:
     def __init__(self, password):
         self.password = password
-    def check_length(self):
-        if len(self.password) >= 12:
-            return "Perfect Length!"
-        else:
-            return "Too Short, likely to be compromised!"
+# Set the class that will be used to check the password for all possible corrections that could be needed
+    def true_uppercase(self):
+        return bool(re.search(r"[A-Z]", self.password))
+    def true_number(self):
+        return bool(re.search(r"[0-9]", self.password))
+    def true_lowercase(self):
+        return bool(re.search(r"[a-z]", self.password))
+    def true_specialchar(self):
+        return bool(re.search(r"[!£$%^&*?<>@#|_]", self.password))
     
+    def is_strong(self):
+        return (
+            len(self.password) >=12 and
+            self.true_uppercase() and
+            self.true_number() and
+            self.true_lowercase() and
+            self.true_specialchar()
+        )
 """
-Set multiple lengths of self.password so that it provides feeback even if it is theoretically long enough to be a good password
-you should still try and get a better length password
+Created a strong password checker that uses all conditions that a regular password checker would use
+like uppercase, special characters and numbers using the very useful regex library
 """
--- passwordStrength.py
+++ passwordStrength.py
@@ -1,6 +1,29 @@
-import re
-from passwordChecker import PasswordStrengthChecker
-password = input("Enter a suitable password: ")
 
-passwordChecker = PasswordStrengthChecker(password)
-print(passwordChecker.check_length())
+from passwordChecker import PasswordStrengthChecker
+while True:    
+    password = input("Enter a suitable password that must include:\n-An uppercase letter!\n-A number!\n-A lowercase letter!\n-A special character!\nEnter Here: ")
+# Long line only includes all the conditions that need to be met    
+    passwordChecker = PasswordStrengthChecker(password)
+    
+    if passwordChecker.is_strong():
+        print("This password is incredibly strong, and is very unlikely to be broken!")
+        print("------------------------------------------------------------------------")
+        break
+    else:
+        print("This password is missing requirements! It must include:")
+
+        if len(password) <12:
+            print("- At least 12 Characters!!")
+        if not passwordChecker.true_uppercase():
+            print("- An uppercase letter!")
+        if not passwordChecker.true_number():
+            print("- A number!")
+        if not passwordChecker.true_lowercase():
+            print("- A lowercase letter!")
+        if not passwordChecker.true_specialchar():
+            print("- A special character!")
+"""
+While loop makes sure that the program doesnt completely stop once a password below the required 12 characters is inputted, will 
+continue to ask for the password until this requirement is met. Made into a nice format that allows the user to see exactly what
+the password needs to include
+"""
