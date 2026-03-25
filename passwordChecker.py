417f8aba1df80d64cdc57e339bfb6407e8af348c
hmeps <hm18@kent.ac.uk>
2026-03-25 13:48:45 +0000

    Commit 3 Added a score system for each condition so that the user will want to create better passwords if it has a game interactive element within the system Added some comments aswell to keep track of my goal with this codespace

--- passwordChecker.py
+++ passwordChecker.py
@@ -2,6 +2,8 @@ import re
 class PasswordStrengthChecker:
     def __init__(self, password):
         self.password = password
+        self.score = 0
+#Adding a scoring method to make the password strength into a game
 # Set the class that will be used to check the password for all possible corrections that could be needed
     def true_uppercase(self):
         return bool(re.search(r"[A-Z]", self.password))
@@ -14,13 +16,33 @@ class PasswordStrengthChecker:
     
     def is_strong(self):
         return (
-            len(self.password) >=12 and
+            len(self.password) >=10 and
             self.true_uppercase() and
             self.true_number() and
             self.true_lowercase() and
             self.true_specialchar()
         )
-"""
+    """
 Created a strong password checker that uses all conditions that a regular password checker would use
 like uppercase, special characters and numbers using the very useful regex library
-"""
+    """
+    def calculate_strength(self):
+        if len(self.password) >=10:
+            self.score +=1
+        if len(self.password) >=12:
+            self.score += 2
+        if len(self.password) >=15:
+            self.score += 3
+        if self.true_uppercase():
+            self.score += 2
+        if self.true_number():
+            self.score += 1
+        if self.true_lowercase():
+            self.score +=1
+        if self.true_specialchar():
+            self.score += 3
+        return self.score
+    """
+The score checker adds a game sense to the regular password checker so that it encourages the user to create stronger and hence safer passwords
+to protect their devices and accounts from potential hacks or data leaks.
+    """
--- passwordStrength.py
+++ passwordStrength.py
@@ -1,19 +1,21 @@
 
 from passwordChecker import PasswordStrengthChecker
-while True:    
-    password = input("Enter a suitable password that must include:\n-An uppercase letter!\n-A number!\n-A lowercase letter!\n-A special character!\nEnter Here: ")
+while True:
+    print("=======================================================================")
+    password = input("Enter a suitable password that must include:\n-At least 10 characters!\n-An uppercase letter!\n-A number!\n-A lowercase letter!\n-A special character!\n||Enter Here: ")
+    print("=======================================================================")
 # Long line only includes all the conditions that need to be met    
     passwordChecker = PasswordStrengthChecker(password)
     
     if passwordChecker.is_strong():
         print("This password is incredibly strong, and is very unlikely to be broken!")
-        print("------------------------------------------------------------------------")
+        print("=======================================================================")
         break
     else:
         print("This password is missing requirements! It must include:")
 
-        if len(password) <12:
-            print("- At least 12 Characters!!")
+        if len(password) <10:
+            print("- At least 10 Characters!!")
         if not passwordChecker.true_uppercase():
             print("- An uppercase letter!")
         if not passwordChecker.true_number():
@@ -25,5 +27,5 @@ while True:
 """
 While loop makes sure that the program doesnt completely stop once a password below the required 12 characters is inputted, will 
 continue to ask for the password until this requirement is met. Made into a nice format that allows the user to see exactly what
-the password needs to include
+the password needs to include.
 """
